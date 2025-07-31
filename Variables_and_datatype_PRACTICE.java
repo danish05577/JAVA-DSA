92b62a676b87b6733d801f2b7e836f240b0fe406
Danish <danish05577@gmail.com>
2025-07-31 23:25:07 +0530

    31-07-2025

---
+++ Variables_and_datatype_PRACTICE.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Variables_and_datatype_PRACTICE{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+//  Question 1 : In a program, input 3 numbers: A, B andC. Youhave to output the average of
+//  these 3 numbers
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int s = a + b + c;
+        // int avg = s/3;
+        // System.out.println(avg);
+
+//  Question 2: In a program, input the side of a square. You have to output the area of the
+//  square.
+        // int side = sc.nextInt();
+        // int area = side * side;
+        // System.out.print("Area:" + area);
+//  Question 3: Enter cost of 3 items from the user (using float data type)- a pencil, a pen and
+//  an eraser. You have to output the total cost of the items back to the user as their bill.
+        // System.out.println("Enter the cost of pencil");
+        // float pencil = sc.nextFloat();
+        // System.out.println("Enter the quantity");
+        // int x = sc.nextInt();
+        // System.out.println("Enter the cost of pen");
+        // float pen = sc.nextFloat();
+        // System.out.println("Enter the quantity");
+        // int y = sc.nextInt();
+        // System.out.println("Enter the cost of erazer");
+        // float eraser = sc.nextFloat();
+        // System.out.println("Enter the quantity");
+        // int z = sc.nextInt();
+        // float cost = pencil*x + pen*y + eraser*z;
+        // System.out.println("total cost:" + cost);
+        // float tax = cost + (0.18f*cost);
+        // System.out.println("Your bill is:"+ tax);
+
+//  Question 5: (Advanced) Will the following statement give any error in Java?
+//  int $ = 24;
+        // int $ = 24;
+        // System.out.print($);
+        
+
+    }
+}
